e37399ecb4afeb4dcfc6319edc4e7daba2395808
justinh <justin-homme@bethel.edu>
2022-04-05 10:22:29 -0500

    clockTick()

--- Clock.c
+++ Clock.c
@@ -11,6 +11,10 @@ clockTick()
   struct Process **myCurProc = myProcTable -> processes[0];
   while(remProcesses > 0) {
       printf("%04d: ", tick);
+      if(myCurProc -> totalTime = 0) {
+          switchState(myCurProc, 'T');
+          printf("%04d: Process ID %d completed", tick, myCurProc -> id);
+      } else if(myCurProc -> )
   }
 
 } // clocktick
