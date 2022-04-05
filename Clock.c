f4db4304a88ec1ffaea290425013654eee9ae17b
Justin Homme <justin-homme@bethel.edu>
2022-04-05 16:34:24 -0500

    clock switch

--- Clock.c
+++ Clock.c
@@ -7,11 +7,33 @@ remProcesses = myProcTable -> numberProc;   // all processes remain
 
 clockTick()
 {
-  tick = 1;
-  struct Process **myCurProc = myProcTable -> processes[0];
-  while(remProcesses > 0) {
-      printf("%04d: ", tick);
-      
-  }
+    tick = 1;
+    struct Process **myCurProc = myProcTable -> processes[0];
+    while(remProcesses > 0) {
+        printf("%04d: ", tick);
+        if(myCurProc -> remainingTime = 0) {
+            switchState(myCurProc, 'T');
+            printf("%04d: Process ID %d moved to T", tick, myCurProc -> id);
+        } else {
+            switch(myCurProc -> curState) {
+                case 'R':
+                    if(myCurProc -> remainingCPU = 0) {
+                        switchState(myCurProc, 'B');
+                        myCurProc -> remainingIO = myCurProc -> ioBurst;
+                        printf("%04d: Process ID %d moved to B", tick, myCurProc -> id);
+                    }
+
+                case 'B':
+                    if(myCurProc -> remainingIO = 0) {
+                        switchState(myCurProc, 'Q');
+                        myCurProc -> remainingCPU = myCurProc -> cpuBurst;
+                        printf("%04d: Process ID %d moved to Q", tick, myCurProc -> id);
+                }
+            }
+
+        }
+
+        tick++;
+    }
 
 } // clocktick
